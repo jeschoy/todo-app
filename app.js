50c4024ef14dca6d1f8df67081256fa3f973dfbd
Jessica Choi <jesymchoi@gmail.com>
2024-01-10 15:51:48 -0800

    added code to prevent default for form and add a new todo

--- app.js
+++ app.js
@@ -2,3 +2,15 @@ const form = document.querySelector('form');
 const submitBtn = document.querySelector('button');
 const inputBox = document.querySelector('input');
 const list = document.querySelector('#todos');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  newTodo();
+})
+
+const newTodo = () => {
+  const newTask = document.createElement('li');
+  newTask.innerText = inputBox.value;
+  list.appendChild(newTask);
+  form.reset();
+}
