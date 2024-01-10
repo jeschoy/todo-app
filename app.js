6a8f3a73d7dbacfacf3610a65c7f34a8f5f8c642
Jessica Choi <jesymchoi@gmail.com>
2024-01-10 15:49:23 -0800

    added code to select elements needed for app

---
+++ app.js
@@ -0,0 +1,4 @@
+const form = document.querySelector('form');
+const submitBtn = document.querySelector('button');
+const inputBox = document.querySelector('input');
+const list = document.querySelector('#todos');
