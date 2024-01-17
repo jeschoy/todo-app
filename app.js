af901e1ab8b471171e3b1d4371a4298779b4930c
Jessica Choi <jesymchoi@gmail.com>
2024-01-17 15:23:07 -0800

    added buttons for deleting tasks

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
 const form = document.querySelector('form');
-const submitBtn = document.querySelector('button');
+const submitBtn = document.getElementsByClassName('.submit');
 const inputBox = document.querySelector('input');
 const list = document.querySelector('#todos');
 
 form.addEventListener('submit', function(e) {
   e.preventDefault();
-  newTodo();
-})
 
-const newTodo = () => {
   const newTask = document.createElement('li');
+  const delBtn = document.createElement('button');
+  delBtn.innerText = 'X'
   newTask.innerText = inputBox.value;
   list.appendChild(newTask);
+  newTask.prepend(delBtn);
   form.reset();
-}
-
-form.addEventListener('click', function(e) {
-  if(e.target.tagName === 'LI') {
-    e.target.style.textDecoration = 'line-through';
-  }
 })
 
-form.addEventListener('dblclick', function(e) {
-  if(e.target.tagName === 'LI') {
-    e.target.remove();
+list.addEventListener('click', function(e) {
+  const selectedItem = e.target.tagName.toLowerCase();
+  if(selectedItem === 'li') {
+    e.target.style.textDecoration = 'line-through';
+  } else if (selectedItem === 'button') {
+    e.target.parentNode.remove();
   }
 })
