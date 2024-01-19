dff7487f1ab369dd7cf69005f8657c53385bdeb8
Jessica Choi <jesymchoi@gmail.com>
2024-01-18 16:21:38 -0800

    moved code for local storage to new file

--- app.js
+++ app.js
@@ -3,37 +3,30 @@ const submitBtn = document.getElementsByClassName('.submit');
 const inputBox = document.querySelector('input');
 const list = document.querySelector('#todos');
 
-const savedList = JSON.parse(localStorage.getItem('todos')) || [];
-for (let i = 0; i < savedList.length; i++) {
-  let newTask = document.createElement('li');
-  newTask.innerText = savedList[i].task;
-  newTask.isCompleted = savedList[i].isCompleted ? true : false;
-  if (newTask.isCompleted) {
-    newTask.style.textDecoration = 'line-through';
-  }
-  list.appendChild(newTask);
-}
-
 form.addEventListener('submit', function(e) {
   e.preventDefault();
 
   const newTask = document.createElement('li');
   const delBtn = document.createElement('button');
-  delBtn.innerText = 'x'
+  delBtn.innerText = 'x';
   newTask.innerText = inputBox.value;
+  newTask.isCompleted = false;
   list.appendChild(newTask);
-  newTask.prepend(delBtn);
+
   form.reset();
 
   savedList.push({ task: newTask.innerText, isCompleted: false });
   localStorage.setItem('todos', JSON.stringify(savedList));
+
+  newTask.prepend(delBtn);
 })
 
 list.addEventListener('click', function(e) {
   const selectedItem = e.target.tagName.toLowerCase();
+  console.log(e.target.isCompleted)
   if(selectedItem === 'li' && !selectedItem.isCompleted) {
     e.target.style.textDecoration = 'line-through';
-    selectedItem.isCompleted = true;
+    e.target.isCompleted = true;
   } else if (selectedItem === 'li' && selectedItem.isCompleted) {
       e.target.style.textDecoration = 'none';
       e.target.isCompleted = false;
@@ -41,3 +34,4 @@ list.addEventListener('click', function(e) {
     e.target.parentNode.remove();
   }
 })
+
---
+++ localstorage.js
@@ -0,0 +1,13 @@
+const savedList = JSON.parse(localStorage.getItem('todos')) || [];
+for (let i = 0; i < savedList.length; i++) {
+  let newTask = document.createElement('li');
+  let delBtn = document.createElement('button');
+  delBtn.innerText = 'x';
+  newTask.innerText = savedList[i].task;
+  newTask.isCompleted = savedList[i].isCompleted ? true : false;
+  if (newTask.isCompleted) {
+    newTask.style.textDecoration = 'line-through';
+  }
+  list.appendChild(newTask);
+  newTask.prepend(delBtn);
+}
